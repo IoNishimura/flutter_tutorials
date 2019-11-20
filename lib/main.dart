7896580e10827bd8ee21018fcbab7e9bd254d6a1
IoNishimura <nishimuraio223@gmail.com>
2019-11-20 23:55:19 +0900

    assetsのフォルダ位置変更、実機ビルド環境構築

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,7 @@ class MyApp extends StatelessWidget {
         body: ListView(
           children: [
             Image.asset(
-              'lib/assets/images/lake.jpg',
+              'assets/images/lake.jpg',
               width: 600,
               height: 240,
               fit: BoxFit.cover,
