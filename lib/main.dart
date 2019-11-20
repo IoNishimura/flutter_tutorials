509291bd7b02b545fd1f111ab9e0b0ee1312d5d7
IoNishimura <nishimuraio223@gmail.com>
2019-11-21 01:02:09 +0900

    不要処理削除

--- lib/main.dart
+++ lib/main.dart
@@ -19,51 +19,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class FavoriteWidget extends StatefulWidget {
-  @override
-  _FavoriteWidgetState createState() => _FavoriteWidgetState();
-}
-
-class _FavoriteWidgetState extends State<FavoriteWidget> {
-  bool _isFavorited = true;
-  int _faboriteCount = 41;
-
-  void _toggleFavorite() {
-    setState(() {
-      if (_isFavorited) {
-        _faboriteCount -= 1;
-        _isFavorited = false;
-      } else {
-        _faboriteCount += 1;
-        _isFavorited = true;
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-            padding: EdgeInsets.all(0),
-            child: IconButton(
-              icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
-              color: Colors.red[500],
-              onPressed: _toggleFavorite,
-            )),
-        SizedBox(
-          width: 18,
-          child: Container(
-            child: Text('$_faboriteCount'),
-          ),
-        )
-      ],
-    );
-  }
-}
-
 // TapboxA manages its own state.
-
 //------------------------- TapboxA ----------------------------------
 class TapboxA extends StatefulWidget {
   TapboxA({Key key}) : super(key: key);
