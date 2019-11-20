bdd69db606dfaec124240719bfb120022c1adafc
IoNishimura <nishimuraio223@gmail.com>
2019-11-20 13:19:00 +0900

    step3終わり

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
     Widget titleScreen = Container(
       padding: const EdgeInsets.all(32),
       child: Row(
@@ -34,6 +36,16 @@ class MyApp extends StatelessWidget {
       ),
     );
 
+    Widget buttonSection = Container(
+        child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    ));
+
     return MaterialApp(
       title: 'Flutter layout demo',
       home: Scaffold(
@@ -41,9 +53,25 @@ class MyApp extends StatelessWidget {
           title: Text('Flutter layout demo'),
         ),
         body: Column(
-          children: [titleScreen],
+          children: [titleScreen, buttonSection],
         ),
       ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8.0),
+          child: Text(label,
+              style: TextStyle(
+                  fontSize: 12, fontWeight: FontWeight.w400, color: color)),
+        )
+      ],
     );
   }
 }
