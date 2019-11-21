887928ded09fba78769c1d7f24d9658ab512d278
IoNishimura <nishimuraio223@gmail.com>
2019-11-22 00:34:09 +0900

    アニメーションのモニタリング

--- lib/main.dart
+++ lib/main.dart
@@ -113,7 +113,14 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
     super.initState();
     controller =
         AnimationController(duration: const Duration(seconds: 2), vsync: this);
-    animation = Tween<double>(begin: 0, end: 300).animate(controller);
+    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controller.forward();
+        }
+      });
     controller.forward();
   }
 
@@ -141,6 +148,5 @@ class AnimatedLogo extends AnimatedWidget {
         child: FlutterLogo(),
       ),
     );
-    ;
   }
 }
